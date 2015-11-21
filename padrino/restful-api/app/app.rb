23584bc5c03c5929319baf8ed9d180e8ebf25813
mario.souza <mario.souza@walmart.com>
2015-11-21 07:17:08 -0200

    commented feature toggle from main app

--- padrino/restful-api/app/app.rb
+++ padrino/restful-api/app/app.rb
@@ -2,10 +2,6 @@ module RestfulApi
   class App < Padrino::Application
     use ConnectionPoolManagement
     register Padrino::Helpers
-    disable :protect_from_csrf
-    disable :layout
-    disable :sessions
-    disable :flash
 
     # enable :sessions
 
@@ -37,13 +33,14 @@ module RestfulApi
     # set :show_exceptions, true    # Shows a stack trace in browser (default for development)
     # set :logging, true            # Logging in STDOUT for development and file for production (default only for development)
     # set :public_folder, 'foo/bar' # Location for static assets (default root/public)
-    set :reload, false            # Reload application files (default in development)
     # set :default_builder, 'foo'   # Set a custom form builder (default 'StandardFormBuilder')
     # set :locale_path, 'bar'       # Set path for I18n translations (default your_apps_root_path/locale)
-    # disable :sessions             # Disabled sessions by default (enable if needed)
-    # disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
     # layout  :my_layout            # Layout can be in views/layouts/foo.ext or views/foo.ext (default :application)
-    #
+    set :reload, false            # Reload application files (default in development)
+    # disable :sessions             # Disabled sessions by default (enable if needed)
+    disable :flash                # Disables sinatra-flash (enabled by default if Sinatra::Flash is defined)
+    disable :protect_from_csrf
+    disable :layout
 
     ##
     # You can configure for a specified environment like:
